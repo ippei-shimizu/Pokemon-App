4773369e70db44c1458afa82ec4ccb942505e945
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-02-16 00:02:22 +0900

    ポケモンをgridレイアウトで表示した

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
-  return <div className="App"></div>;
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      // 各ポケモンの詳細データを取得
+      loadPokemon(res.results);
+
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>loading</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, index) => {
+                return <Card key={index} pokemon={pokemon} />;
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
